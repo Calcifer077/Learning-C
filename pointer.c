8fab975ff16f644ccd2a36e253be4281006f864d
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2022-12-19 19:25:02 +0530

    Create pointer.c

---
+++ pointer.c
@@ -0,0 +1,29 @@
+//Pointer are declared by using '*' in front of the variable name.
+//They store the value of memory address of other variabe.
+//It points to the same data type same as its own. Like int pointer will print memory address of only int type.
+//TO print the memory address we use '&' in front of the variable naem.
+
+//From google>
+int myAge = 43;     // An int variable
+int* ptr = &myAge;  // A pointer variable, with the name ptr, that stores the address of myAge
+
+// Output the value of myAge (43)
+printf("%d\n", myAge);
+
+// Output the memory address of myAge (0x7ffe5367e044)
+printf("%p\n", &myAge);
+
+// Output the memory address of myAge with the pointer (0x7ffe5367e044)
+printf("%p\n", ptr);
+.........>
+#include<stdio.h>
+
+int main(){
+	int age = 10;
+	int *ptr = &age;
+	printf("age = %d \n", age); //prints the value of age
+	printf("&age = %d  \n", &age); //prints the memory address of age
+	printf("ptr = %d \n", ptr); //prints the value of age
+	printf("*ptr = %d  \n", *ptr); //pritns the memory address of age
+	return 0;
+}
